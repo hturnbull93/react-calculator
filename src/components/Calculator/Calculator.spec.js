94e54ca999bc6c0b96b4b833f23ba75715ea9aba
Harry Turnbull <hturnbull@live.co.uk>
2020-06-12 13:06:03 +0100

    R: Calculator renders a Display using its displayValue

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Calculator from "./Calculator";
-import Display from '../Display/Display';
+import Display from "../Display/Display";
 
 describe("Calculator", () => {
   it("renders a div", () => {
     const wrapper = shallow(<Calculator />);
     expect(wrapper.find("div").length).toEqual(1);
   });
-  
-  it('renders the Display Component', () => {
+
+  it("renders the Display Component", () => {
     const wrapper = shallow(<Calculator />);
-    expect(wrapper.containsMatchingElement(<Display />)).toEqual(true);
+    const displayValue = wrapper.instance().state.displayValue;
+    const expected = <Display displayValue={displayValue} />;
+    expect(wrapper.containsMatchingElement(expected)).toEqual(true);
   });
 });

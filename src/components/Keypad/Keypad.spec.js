9f90385021abb2d15bd0a8ddf12e6f56742143cd
Harry Turnbull <hturnbull@live.co.uk>
2020-06-12 16:47:40 +0100

    R: Keypad renders the values of numbers, fix other test

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -3,9 +3,15 @@ import { shallow } from 'enzyme';
 import Keypad from './Keypad';
 
 describe('Keypad', () => {
-  it('renders a div', () => {
+  it('renders 2 divs', () => {
     const wrapper = shallowKeypad()
-    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('div').length).toEqual(2);
+  });
+  
+  it('renders the values of numbers', () => {
+    const wrapper = shallowKeypad()
+    wrapper.setProps({numbers: ['0', '1', '2']});
+    expect(wrapper.find('.numbers-container').text()).toEqual('012');
   });
 });
 

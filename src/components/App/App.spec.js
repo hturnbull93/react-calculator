7070d4bd36f7d335d9148631e3a81e11a15a239d
Harry Turnbull <hturnbull@live.co.uk>
2020-06-12 12:11:37 +0100

    RN tests

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -4,12 +4,12 @@ import App from './App';
 import Calculator from '../Calculator/Calculator'
 
 describe('App', () => {
-  it('should render a <div />', () => {
+  it('renders a div', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('div').length).toEqual(1);
   });
   
-  it('should render the Calculator Component', () => {
+  it('renders the Calculator Component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
   });

e3a6700210df90d832e463f366a27a4ab2874443
Harry Turnbull <hturnbull@live.co.uk>
2020-06-13 19:04:58 +0100

    R: Keypad uses Key for numbers operators submit

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Keypad from './Keypad';
+import React from "react";
+import { shallow } from "enzyme";
+import Keypad from "./Keypad";
 
-describe('Keypad', () => {
-  it('renders 3 divs', () => {
-    const wrapper = shallowKeypad()
-    expect(wrapper.find('div').length).toEqual(3);
-  });
-  
-  it('renders the values of numbers', () => {
-    const wrapper = shallowKeypad()
-    wrapper.setProps({numbers: ['0', '1', '2']});
-    expect(wrapper.find('.numbers-container').text()).toEqual('012');
+describe("Keypad", () => {
+  it("renders 3 divs", () => {
+    const wrapper = shallowKeypad();
+    expect(wrapper.find("div").length).toEqual(3);
   });
 
-  it('renders the values of operators', () => {
-    const wrapper = shallowKeypad()
-    wrapper.setProps({operators: ['+', '-', '*', '/']});
-    expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
+  it("renders the values of numbers", () => {
+    const wrapper = shallowKeypad();
+    wrapper.setProps({ numbers: ["0", "1", "2"] });
+    expect(wrapper.find(".numbers-container").text()).toEqual("012");
   });
-  
-  it('renders a Key component', () => {
-    const wrapper = shallowKeypad()
-    expect(wrapper.find('Key').length).toEqual(1)
+
+  it("renders the values of operators", () => {
+    const wrapper = shallowKeypad();
+    wrapper.setProps({ operators: ["+", "-", "*", "/"] });
+    expect(wrapper.find(".operators-container").text()).toEqual("+-*/");
+  });
+
+  it("renders a Key component for each of numbers and operators, and submit", () => {
+    const wrapper = shallowKeypad();
+    const numbers = ["0", "1"];
+    const operators = ["+", "-"];
+    const submit = 1;
+    const keyTotal = numbers.length + operators.length + submit;
+    wrapper.setProps({ numbers, operators });
+    expect(wrapper.find("Key").length).toEqual(keyTotal);
   });
 });
 
